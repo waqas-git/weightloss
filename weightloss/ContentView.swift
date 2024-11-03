87438309f808b2c26c6701e2451f7ec765a84a65
Waqas Ahmed <waqasahmed.uol@gmail.com>
2024-11-03 13:10:37 +0500

    Initial Commit

---
+++ weightloss/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weightloss
+//
+//  Created by waqas ahmed on 03/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weightloss/weightlossApp.swift
@@ -0,0 +1,17 @@
+//
+//  weightlossApp.swift
+//  weightloss
+//
+//  Created by waqas ahmed on 03/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct weightlossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
